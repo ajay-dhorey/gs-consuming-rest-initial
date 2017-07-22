e4e3b0bc01857dfdaa55f0b145a5dc8daf8b9d32
Ajay Dhorey <Ajay Dhorey>
2017-07-22 13:31:42 +0530

    added post method

--- src/main/java/hello/HeroController.java
+++ src/main/java/hello/HeroController.java
@@ -2,6 +2,7 @@
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,29 @@ public class HeroController {
 			}
 			
 		} 
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.PUT, value= "/api/heroes")
+	public Hero updateHero( @RequestBody Hero hero){
+		System.out.println( String.format("in add Heroes %s", hero));
+		Long id = hero.getId();
+		Hero updateHero = null;
+		if(id != null) {
+			updateHero = heroRepository.save(hero);
+			
+		} 
+		return updateHero;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value= "/api/heroes" )
+	public Hero addHero( @RequestBody Hero hero){
+		System.out.println( String.format("in add Heroes %s", hero));
+		Hero updateHero = null;
+		//if(hero != null && hero.getName() != null) {
+			updateHero = heroRepository.save(new Hero(hero.getName()));
+		//} 
+		return updateHero;
 	}
 	
 	
